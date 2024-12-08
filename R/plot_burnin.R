6eb767d6be6a40cebc870ce6239a30d51fccf5bb
Atsusaka <yatsusak@CougarNet.UH.EDU>
2024-12-08 00:19:21 -0600

    visualize burn-in period

--- R/plot_burnin.R
+++ R/plot_burnin.R
@@ -1,20 +1,22 @@
-#' Plot candiate behavior inside and outside the burnin period
+#' Plot candidate behavior inside and outside the burn-in period
 #'
-#' @description `plot_burnin()` visualizes the sequence of candiate positions.
+#' @description `plot_burnin()` visualizes the sequence of candidate positions.
 #'
 #' @param out Output from `neodowns`
-#'  @return
-#' @importFrom dplyr case_when `%>%` mutate select left_join rename tibble filter bind_rows
-#' @importFrom ggplot2 ggplot
+#' @param n_iter Number of iterations to plot
+#' @return ggplot output
+#' @importFrom dplyr `%>%` filter
+#' @importFrom ggplot2 ggplot aes geom_point geom_hline geom_vline scale_size scale_color_manual scale_shape_manual facet_wrap ylim xlim theme_minimal
 #' @export
 
 # library(gganimate)
 # library(gapminder)
 # library(ggplot2)
 
-plot_burnin <- function(out){
+plot_burnin <- function(out,
+                        n_iter){
 
-n_iter <- 100
+
 out_cands <- out$cands %>%
   filter(iter <= n_iter)
 
@@ -27,60 +29,19 @@ ggplot(out_cands, aes(x = x, y = y,
   scale_size(range = c(2, 10)) +
   scale_color_manual(
     values = c(
-      alpha("deepskyblue4", 0.8),
-      alpha("deeppink4", 0.8),
-      alpha("slategray", 0.8),
-      alpha("deepskyblue4", 0.8),
-      alpha("deeppink4", 0.8),
-      alpha("slategray", 0.8)
-    )) +
+      "deepskyblue4",
+      "deeppink4",
+      "slategray",
+      "deepskyblue4",
+      "deeppink4",
+      "slategray")
+    ) +
   scale_shape_manual(values = c(1, 2, 0, 16, 17, 15)) +
   facet_wrap(~ system, ncol = 2) +
   ylim(-4, 4) +
   xlim(-4, 4) +
-  theme_minimal()
+  theme_minimal() -> p
 
-
-q <- extract(out, quantity = "ideology")
-
-ggplot(q, aes(y = `mean(moderation)`, x = iter)) +
-  geom_point() +
-  facet_wrap(~ system, ncol = 2)
-
-#
-#
-#   ggplot(out_cands, aes(x = x, y = y,
-#                   size = count,
-#                   colour = party, shape = factor(party))) +
-#     geom_point(alpha = 0.7, show.legend = F) +
-#     geom_hline(aes(yintercept = 0), color = "black", linetype = "dashed") +
-#     geom_vline(aes(xintercept = 0), color = "black", linetype = "dashed") +
-#     scale_size(range = c(2, 10)) +
-#     scale_color_manual(
-#       values = c(
-#         alpha("deepskyblue4", 0.8),
-#         alpha("deeppink4", 0.8),
-#         alpha("slategray", 0.8),
-#         alpha("deepskyblue4", 0.8),
-#         alpha("deeppink4", 0.8),
-#         alpha("slategray", 0.8)
-#       )) +
-#     scale_shape_manual(values = c(1, 2, 0, 16, 17, 15)) +
-#     facet_wrap(~ type, ncol = 2) +
-#     ylim(-3, 3) +
-#     xlim(-3, 3) +
-#     theme_minimal() +
-#     # This is where gganimation begins
-#     labs(title = 'Iteration: {frame_time}',
-#          x = 'First dimension', y = 'Second dimension') +
-#     transition_time(as.integer(iter)) +
-#     ease_aes('quadratic-in') -> gif
-#
-#
-#   gif
-#
-#   anim_save("animation.gif", gif)
-#
-
+return(p)
 
 }
